1af142eab31f6c537ae0a7990b56d97282032fb0
eriksmedeiros <erik.medeiros.701@ufrn.edu.br>
2025-05-12 23:31:04 -0300

    created form for new products

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,15 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
   isSidePanelVisible: boolean = false;
+  productObj: any = {
+    "ProductId": 0,
+    "ProductSku": "",
+    "ProductName": "",
+    "ProductPrice": 0,
+    "ProductShortName": "",
+    "ProductDescription": "",
+    "CreatedDate": new Date(),
+    "DeliveryTimeSpan": "",
+    "CategoryId": 0,
+    "ProductImageUrl": "",
+  }
 
   openSidePanel() {
     this.isSidePanelVisible = true;
