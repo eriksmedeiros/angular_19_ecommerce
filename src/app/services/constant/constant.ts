74ed5591bc356db10cd4f5ca06316b7ffd12b37f
eriksmedeiros <erik.medeiros.701@ufrn.edu.br>
2025-05-13 23:50:06 -0300

    connect to API consumption

---
+++ src/app/services/constant/constant.ts
@@ -0,0 +1,8 @@
+export const Constant = {
+  API_END_POINT: '/api/api/BigBasket/',
+  METHODS: {
+    GET_ALL_CATEGORIES: 'GetAllCategory',
+    GET_ALL_PRODUCTS: 'GetAllProducts',
+    CREATE_PRODUCT: 'CreateProduct'
+  }
+}
